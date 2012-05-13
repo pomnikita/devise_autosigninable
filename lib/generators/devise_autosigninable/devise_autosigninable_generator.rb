0db4368406a8ebb91f9851ce1eeb91d2d88c929b
Marat Galiev <kazanlug@gmail.com>
2012-05-13 17:13:51 +0400

    Move generators dir

---
+++ lib/generators/devise_autosigninable/devise_autosigninable_generator.rb
@@ -0,0 +1,24 @@
+require 'rails/generators'
+require 'rails/generators/migration'
+
+class DeviseAutosigninableGenerator < Rails::Generators::NamedBase
+	
+	source_root File.expand_path('../templates', __FILE__)	
+	
+	include Rails::Generators::Migration
+
+	def self.next_migration_number(dirname)
+        if ActiveRecord::Base.timestamped_migrations
+          migration_number = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+          migration_number += 1
+          migration_number.to_s
+        else
+          "%.3d" % (current_migration_number(dirname) + 1)
+        end
+    end
+
+    def copy_migration    	
+        migration_template 'migration.rb', "db/migrate/add_autosigninable_to_#{table_name}"
+    end
+
+end
