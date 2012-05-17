12e5eab82b770a7af306af42f29d8c3edd6ed97e
Nikita Pomyashchiy <pomnikita@gmail.com>
2012-05-17 14:56:28 +0400

    Remove schema.rb as deprecated

--- lib/generators/devise_autosigninable/templates/migration.rb
+++ lib/generators/devise_autosigninable/templates/migration.rb
@@ -1,7 +1,7 @@
 class AddAutosigninableTo<%= table_name.camelize %> < ActiveRecord::Migration
   def self.up
     change_table :<%= table_name %> do |t|
-      t.autosigninable
+      t.string :autosignin_token, :limit => 40
     end
     add_index :<%= table_name %>, :autosignin_token, :unique => true
   end
